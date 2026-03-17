d046e756379de49ff80c1eeabee80ecb2f0d3b68
Joyal <joyalksdev@gmail.com>
2026-03-17 14:00:17 +0530

    Fix: fixed the same site fetching issue

--- utils/sendTokenResponse.js
+++ utils/sendTokenResponse.js
@@ -7,12 +7,12 @@ const sendTokenResponse = (user, statusCode, res) => {
   });
 
   // Cookie settings
-  const cookieOptions = {
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
-  };
+    const cookieOptions = {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true, 
+        sameSite: "None", 
+    };
 
   // Send response
   res
